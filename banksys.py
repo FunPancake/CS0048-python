d62e0f9dd667dce4efcec0a47fc19cded7dcee0d
Ian Lindley <92287632+FunPancake@users.noreply.github.com>
2025-05-21 09:35:43 +0800

    Finished banksys.py

--- banksys.py
+++ banksys.py
@@ -24,55 +24,59 @@ deposit/withraw -> array -> checkbalance -> transaction log -> menu
 """
 history = []
 class Bank:
-    def __init__(self, balance, amount):
-        self.balance = balance
-        self. amount = amount
+    def __init__(self):
+        self.balance = 0
+        self.amount = 0
         print('Blazibank Banking System')
         
-    def check_balance(self, balance):
-        print(f"balance is: {self.balance} ")
+    def check_balance(self):
+        print(f"Your Current Balance is: ", self.balance)
         
-    def deposit(self, balance, amount):
-        self.amount = float(input("Enter deposit amount: "))
-        self.balance += self.amount
-        print("\n Deposited: ", self.amount)
-        history.append("Deposit ", "$", self.amount)
+    def deposit(self):
+        amount = float(input("Enter Deposit amount: "))
+        self.balance += amount
+        print("\nDeposited: ", amount)
+        history.append("Deposit: " + str(amount))
         
-    def withraw():
-        amount = float(input("Enter withdrawal amount: "))
-        balance -= amount
-        print("\n Withrawn: ", amount)
-        history.append("Withraw, ", "$",amount)
-
-    def v_history():
-        # for i, item in enumerate(history):
-        #     print (f"{i+1}. {item}", sep="\n")
-        print('list')
-        
-    while True:
-        print('Menu: ')
-        print('1. Check Balance')
-        print('2. Deposit')
-        print('3. Withraw')
-        print('4. View History')
-        print('5. Exit')
-
-        choice = input('Enter your choice(1-5): ')
-        if choice == "1":
-            check_balance(self, balance)
-            
-        elif choice == "2":
-            deposit()
-            
-        elif choice == "3":
-            withraw()
-            
-        
-        elif choice == "4":
-            v_history()
-            
-        elif choice == "5":
-            print('Thank you!')
-            break
+    def withraw(self):
+        amount = float(input("Enter Withraw amount: "))
+        if self.balance >= amount:
+            self.balance -= amount 
+            print("\nWithrawn: ", str(amount))
+            history.append("Withraw: " + str(amount))
         else:
-            print('Invalid Choice!')
+            print("Insufficient Balance!")
+        
+
+    def v_history(self):
+        print("\nList of Transaction: ")
+        for i, item in enumerate(history):
+            print (f"{i+1}. {item}", sep="\n")
+        
+s = Bank()
+while True:
+    print('\n\nMenu: ')
+    print('1. Check Balance')
+    print('2. Deposit')
+    print('3. Withraw')
+    print('4. View History')
+    print('5. Exit')
+
+    choice = input('Enter your choice(1-5): ')
+    if choice == "1":
+        s.check_balance()
+        
+    elif choice == "2":
+        s.deposit()
+        
+    elif choice == "3":
+        s.withraw()
+        
+    elif choice == "4":
+        s.v_history()
+        
+    elif choice == "5":
+        print('Thank you!')
+        break
+    else:
+        print('Invalid Choice!')
