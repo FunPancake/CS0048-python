7e0788175b33166feff852ebb610bd1c2ad3801c
Ian Lindley <92287632+FunPancake@users.noreply.github.com>
2025-06-18 07:52:32 +0800

    Add files via upload

---
+++ Data type/datatype.py
@@ -0,0 +1,18 @@
+n_list = ["happy", [2, 0 , 1, 5]]
+# print(n_list[1][3])
+
+
+my_list = ['H', 'A', 'P', 'P', 'Y']
+# print(my_list[-3])
+
+mult_list = [['B','A','R','K'], ['L','E','A','F'], ['G','R','A','S','S'], ['S','O','I','L']]
+# print(mult_list[3][2])
+x = mult_list.count('A')
+print(x)
+
+num_list = [1,3,5,7,9]
+# num_list.insert(1,2)
+# del num_list
+# num_list.remove(3)
+# print(num_list.pop())
+print(num_list)
