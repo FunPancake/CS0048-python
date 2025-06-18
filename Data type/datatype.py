018d47d34b0b7112365407430383df43d631dc1d
Ian Lindley <92287632+FunPancake@users.noreply.github.com>
2025-06-18 08:11:36 +0800

    cleaned view no update

--- Data type/datatype.py
+++ Data type/datatype.py
@@ -1,18 +1,23 @@
 n_list = ["happy", [2, 0 , 1, 5]]
-# print(n_list[1][3])
+print(n_list[1][3])
 
 
 my_list = ['H', 'A', 'P', 'P', 'Y']
-# print(my_list[-3])
+print(my_list[-3])
 
-mult_list = [['B','A','R','K'], ['L','E','A','F'], ['G','R','A','S','S'], ['S','O','I','L']]
-# print(mult_list[3][2])
-x = mult_list.count('A')
+mult_list = [['B','A','R','K'], 
+            ['L','E','A','F'], 
+            ['G','R','A','S','S'], 
+            ['D','I','R','T']]
+print(mult_list[3][2])
+x = sum(sublist.count('A') for sublist in mult_list)
 print(x)
+print(mult_list)
 
 num_list = [1,3,5,7,9]
-# num_list.insert(1,2)
-# del num_list
-# num_list.remove(3)
-# print(num_list.pop())
+num_list.insert(1,2)
+num_list.remove(3)
+print(num_list.pop())
 print(num_list)
+
+# del num_list
