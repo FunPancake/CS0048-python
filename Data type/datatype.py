97a4c9378f3ae19e989dd4ca7ddb0568fdbad675
Ian Lindley <92287632+FunPancake@users.noreply.github.com>
2025-06-18 08:33:57 +0800

    Multiple updates
    Added sub topic separator
    added string escape castings

--- Data type/datatype.py
+++ Data type/datatype.py
@@ -1,9 +1,12 @@
 n_list = ["happy", [2, 0 , 1, 5]]
 print(n_list[1][3])
 
+print('\n\n') #-----------------------------------------------------------------
 
 my_list = ['H', 'A', 'P', 'P', 'Y']
 print(my_list[-3])
+
+print('\n\n')
 
 mult_list = [['B','A','R','K'], 
             ['L','E','A','F'], 
@@ -12,10 +15,12 @@ mult_list = [['B','A','R','K'],
 print(mult_list[3][2])
 x = sum(sublist.count('A') for sublist in mult_list)
 print(x)
-print(mult_list)
+# print(mult_list)
 
 for sublist in mult_list:
     print(sublist)
+
+print('\n\n') #-----------------------------------------------------------------
 
 num_list = [1,3,5,7,9]
 num_list.insert(1,2)
@@ -25,4 +30,11 @@ print(num_list)
 
 # del num_list
 
+print('\n\n') #-----------------------------------------------------------------
+
 print ("I asked, \"What's my name\"") # \"(string)\"
+
+print('\n\n') #-----------------------------------------------------------------
+
+print("This is \x61 \ngood example")
+print(r"This is \x61 \ngood example")
